f338d86c3fe08cb8d03bc735e473c16f1feb2e84
Emma Vasylyk <emma.ovsiannikova91@gmail.com>
2021-12-05 00:12:13 +0200

    hw2-1

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import FeedbackOptions from './components/FeedbackOptions';
+import Section from './components/Section';
+import Statistics from './components/Statistics';
+import Notification from './components/Notification';
+import './App.css';
+
+const WrapperFeedback = ['WrapperFeedback'];
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    const result = good + neutral + bad;
+    return result;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    return Math.round((good / this.countTotalFeedback()) * 100) || 0;
+  };
+
+  onLeaveFeedback = e => {
+    const name = e.target.name;
+    this.setState(prevState => ({
+      [name]: prevState[name] + 1,
+    }));
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const objKey = Object.keys(this.state);
+
+    return (
+      <div className={WrapperFeedback}>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={objKey}
+            onLeaveFeedback={this.onLeaveFeedback}
+          />
+        </Section>
+        {this.countTotalFeedback() === 0 ? (
+          <Notification message="There is no feedback" />
+        ) : (
+          <Section title="Statistics">
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          </Section>
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
